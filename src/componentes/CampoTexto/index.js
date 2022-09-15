4b57f75f5a4a3abb3fce10fc200902e167e300af
Eduardo Portella <eportella70@gmail.com>
2022-09-15 18:32:22 -0300

    campos de texto

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,12 @@
+import './index.css';
+
+const CampoTexto = () => {
+    return (
+        <div className='campotexto'>
+            <label>Nome</label>
+            <input placeholder="Digite o seu nome"/>
+        </div>
+    );
+};
+
+export default CampoTexto;
