388e7d26c07e15970f5c03d664b824274312d6aa
Eduardo Portella <eportella70@gmail.com>
2022-10-12 16:10:06 -0300

    manipulando input

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,18 @@
 import './index.css';
 
 const CampoTexto = (props) => {
+
+    let valor = '';
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value;
+        console.log(valor);
+    };
+
     return (
         <div className='campotexto'>
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}/>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     );
 };
